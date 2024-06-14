4b4d97180d9423e396d8eadbbee3f004edf08d73
Ahed Eisa <78013422+be-great@users.noreply.github.com>
2024-06-14 20:18:35 +0300

    add init file

---
+++ init.sh
@@ -0,0 +1,9 @@
+if [ ! -d "venv" ]; then
+    echo --------------------
+    echo Creating virtualenv
+    echo --------------------
+    virtualenv venv
+fi
+source venv/bin/activate
+
+pip install -r requirements.txt
