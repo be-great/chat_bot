1e22a662f37e3fbefa20b84dc24e353415ff3049
be-great <begreat5333@gmail.com>
2024-06-15 22:24:25 +0300

    init files

--- init.sh
+++ init.sh
@@ -7,6 +7,13 @@ fi
 source venv/bin/activate
 
 pip install -r requirements.txt
-# run this at the 
-rasa run &
-python3 train_data.py
+# Run rasa in the background using nohup
+nohup rasa run > rasa_output.log 2>&1 &
+
+# Run train_data.py in the background using nohup
+nohup python3 train_data.py > train_data_output.log 2>&1 &
+
+# Wait for both processes to start
+sleep 2
+
+echo "Both rasa and train_data.py are running in the background."
